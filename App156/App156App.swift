101704a52eb3274e51fae508bf34632e8d48c9d1
spustoff <117011783+spustoff@users.noreply.github.com>
2023-08-07 13:40:09 +0300

    Initial Commit

---
+++ App156/App156App.swift
@@ -0,0 +1,17 @@
+//
+//  App156App.swift
+//  App156
+//
+//  Created by Вячеслав on 8/7/23.
+//
+
+import SwiftUI
+
+@main
+struct App156App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
